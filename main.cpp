8303a5ddc520132e60d6a991a1d19c924b98ad45
AJSbrar100 <98273093+AJSbrar100@users.noreply.github.com>
2025-07-21 23:11:24 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include "raylib.h"
+#include <iostream>
+using namespace std;
+
+const int WIDTH = 800;
+const int HEIGHT = 800;
+
+const int FPS = 30;
+
+int x, y;
+
+int i = 0;
+
+int main() {
+	InitWindow(WIDTH, HEIGHT, "Wave visual");
+	SetTargetFPS(FPS);
+
+	while (!WindowShouldClose() && i < WIDTH) {
+
+		x = i;
+		y = (HEIGHT / 2) + sinf(i * (PI / 180)) * 100;
+
+		BeginDrawing();
+
+		DrawPixel(x, y, WHITE);
+
+		EndDrawing();
+
+		i++;
+	}
+
+	CloseWindow();
+	return 0;
+}
